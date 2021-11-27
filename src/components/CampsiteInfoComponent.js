08f508e8cd1c03b7cda4fcaa1f10fe2fb5d6c9c3
Ashley Blemler <ashleyblemler@gmail.com>
2021-11-26 19:57:08 -0800

    Week 4 Assignment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Button,
   Card,
   CardImg,
   CardText,
@@ -7,8 +8,13 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
 
 function RenderCampsite({ campsite }) {
   return (
@@ -39,11 +45,11 @@ function RenderComments({ comments }) {
             }).format(new Date(Date.parse(comments.date)))}
           </div>
         ))}
+        <CommentForm />
       </div>
     );
-  } else {
-    return <div />;
-  }
+  } 
+  return <div />;
 }
 
 function CampsiteInfo(props) {
@@ -71,5 +77,102 @@ function CampsiteInfo(props) {
   }
   return <div />;
 }
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+  }
+
+  toggleModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
+
+  handleSubmit = values => {
+    console.log('Current state is: ' + JSON.stringify(values));
+    alert('Current state is: ' + JSON.stringify(values));
+  };
+
+  render() {
+    return (
+      <>
+        <Button outline onClick={this.toggleModal}>
+          <i className="fa fa-pencil" />
+          Submit Comment
+        </Button>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+              <div className="form-group">
+                <Label htmlFor="rating">Rating</Label>
+                <Control.select
+                  model=".rating"
+                  name="rating"
+                  className="form-control"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="author">Your Name</Label>
+                <Control.text
+                  model=".author"
+                  id="author"
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: 'Required',
+                    minLength: 'Must be at least 2 characters',
+                    maxLength: 'Must be 15 characters or less',
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <Label htmlFor="text">Comment</Label>
+                <Control.textarea
+                  model=".text"
+                  id="text"
+                  name="text"
+                  rows="6"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <Button type="submit" color="primary">
+                  Submit
+                </Button>
+              </div>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </>
+    );
+  }
+}
+
 
 export default CampsiteInfo;
